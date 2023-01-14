ec7d19d0399d1ff7e54066284ba3f08c699444aa
tim1504 <109680883+tim1504@users.noreply.github.com>
2023-01-14 16:21:32 +0100

    implement random simulation

--- src/logic.rs
+++ src/logic.rs
@@ -9,12 +9,57 @@ pub struct OwnLogic;
 
 impl GameClientDelegate for OwnLogic {
     fn request_move(&mut self, state: &State, _my_team: Team) -> Move {
+
         info!("Requested move");
-        let chosen_move = *state.possible_moves()
-            .choose(&mut rand::thread_rng())
-            .expect("No move found!");
-        info!("Chose move {}", chosen_move);
-        chosen_move
+        let simulations = 10000/state.possible_moves().len();
+
+        let mut best_move: Option<Move> = None;
+        let mut best_score = i32::MIN;
+
+        for possible_move in state.possible_moves() {
+
+            let score = {
+                let mut sum = 0;
+                for _i in 0..simulations {
+                    sum += random_simulation(state.clone(), &state.current_team())
+                }
+                sum
+            };
+
+            if score > best_score {
+                best_move = Some(possible_move);
+                best_score = score;
+            }
+
+        }
+
+        if best_move.is_none() {
+            panic!("No best move could be found!");
+        }
+
+        fn random_simulation( mut gs: State, team: &Team) -> i32 {
+            while !gs.is_over() {
+                let random_move = *gs.possible_moves()
+                    .choose(&mut rand::thread_rng())
+                    .expect("No move found!");
+
+                gs.perform(random_move);
+            }
+            if gs.winner().is_none() {
+                return 0
+            }
+            if gs.winner().unwrap().eq(team) {
+                return 1
+            }
+            -1
+        }
+
+        best_move.unwrap()
+
+    }
+
+    fn on_game_end(&mut self, _result: &socha_client_2023::protocol::GameResult) {
+        //do sth
     }
 
     fn on_update_state(&mut self, state: &State) {
