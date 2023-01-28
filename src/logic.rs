d87eddfea2d669caef16638652569a2a7b48b1e7
tim1504 <109680883+tim1504@users.noreply.github.com>
2023-01-28 20:32:18 +0100

    Implement Monte Carlo Tree Search

--- src/logic.rs
+++ src/logic.rs
@@ -1,60 +1,36 @@
 use log::{info, debug};
 use rand::seq::SliceRandom;
+use std::time;
 
 use socha_client_2023::{client::GameClientDelegate, game::{Move, Team, State}};
 
-/// An empty game logic structure that implements the client delegate trait
-/// and thus is responsible e.g. for picking a move when requested.
+
 pub struct OwnLogic;
 
 impl GameClientDelegate for OwnLogic {
     fn request_move(&mut self, state: &State, _my_team: Team) -> Move {
 
         info!("Requested move");
-        let simulations = 10000/state.possible_moves().len();
 
-        let mut best_move: Option<Move> = None;
-        let mut best_score = i32::MIN;
+        //Create root node
+        let mut root = Node::new(state.clone());
+        //Expand root node
+        root.expand();
 
-        for possible_move in state.possible_moves() {
+        //Runs for 2 seconds
+        let start = time::Instant::now();
 
-            let score = {
-                let mut sum = 0;
-                for _i in 0..simulations {
-                    sum += random_simulation(state.clone(), &state.current_team())
-                }
-                sum
-            };
+        let mut c = 0;
 
-            if score > best_score {
-                best_move = Some(possible_move);
-                best_score = score;
-            }
-
+        while start.elapsed().as_millis() < 1800 {
+            c += 1;
+            root.mcts();
         }
 
-        if best_move.is_none() {
-            panic!("No best move could be found!");
-        }
+        println!("Rounds: {}", c);
 
-        fn random_simulation( mut gs: State, team: &Team) -> i32 {
-            while !gs.is_over() {
-                let random_move = *gs.possible_moves()
-                    .choose(&mut rand::thread_rng())
-                    .expect("No move found!");
-
-                gs.perform(random_move);
-            }
-            if gs.winner().is_none() {
-                return 0
-            }
-            if gs.winner().unwrap().eq(team) {
-                return 1
-            }
-            -1
-        }
-
-        best_move.unwrap()
+        //Return best move
+        root.select_child().state.last_move().unwrap()
 
     }
 
@@ -65,4 +41,121 @@ impl GameClientDelegate for OwnLogic {
     fn on_update_state(&mut self, state: &State) {
         debug!("Board:\n{}", state.board());
     }
+    
+    
 }
+
+struct Node {
+    state: State,
+    children: Vec<Node>,
+    visits: u32,
+    total: i32,
+}
+
+impl Node {
+
+    fn new(state: State) -> Self {
+        Node {
+            state,
+            children: Vec::new(),
+            visits: 0,
+            total: 0,
+        }
+    }
+
+    fn mcts(&mut self) -> i32 {
+        let mut result = 0;
+        if self.visits > 0 && !self.state.is_over(){
+            if self.children.is_empty() {
+                self.expand();
+            }
+            let selected_child = self.select_child();
+            result = selected_child.mcts();
+        } else {
+            for _i in 0..100 {
+                result += self.rollout(self.state.current_team());
+            }
+        }
+        self.visits += 1;
+        self.total += result;
+        return result;
+    }
+    
+    fn select_child(&mut self) -> &mut Node {
+        let mut best_score = f64::MIN;
+        let mut best_child = None;
+        for child in self.children.iter_mut() {
+            let mut score = (child.total as f64 / child.visits as f64) + (2.0*(self.visits as f64).ln() / child.visits as f64).sqrt();
+            if child.visits == 0 {
+                score = f64::MAX;
+            }
+            if score > best_score {
+                best_child = Some(child);
+                best_score = score;
+            }
+        }
+        best_child.unwrap()
+    }
+
+    fn expand(&mut self) {
+        for m in self.state.possible_moves() {
+            let mut next_state = self.state.clone();
+            next_state.perform(m);
+            self.children.push(Node::new(next_state));
+        }
+    }
+
+    fn rollout(&mut self, team: Team) -> i32 {
+        let mut state = self.state.clone();
+        while !state.is_over() {
+            let random_move = *state.possible_moves()
+                .choose(&mut rand::thread_rng())
+                .expect("No move found!");
+            state.perform(random_move);
+        }
+        (state.fish(team) - state.fish(team.opponent())) as i32
+        /*
+        if state.winner().is_none() {
+            return 0
+        }
+        if state.winner().unwrap().eq(&team) {
+            return 1
+        }
+        -1
+        */
+    }
+
+}
+
+
+            /* 
+            //Selection
+            while !current.children.is_empty() {
+                //Select child with highest UCT
+                current = current.select_child();
+            }
+
+            //Expansion
+            if current.visits > 0 {
+                current.expand();
+                current = current.select_child();
+            }
+
+            //Rollout
+            let mut sum = 0;
+            for _i in 0..100 {
+                sum += current.rollout(state.current_team());
+            }
+
+            current.visits += 1;
+            current.total += sum;
+
+            //Backpropagate
+            while current.parent.is_some() {
+                current = *current.parent.unwrap();
+                current.visits += 1;
+                current.total += sum;
+            }
+            
+        }
+        */
