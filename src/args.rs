d80e064135f158b93c489790b9164d95cca437c5
tim1504 <109680883+tim1504@users.noreply.github.com>
2023-02-23 13:17:35 +0100

    Add args for testing

--- src/args.rs
+++ src/args.rs
@@ -3,6 +3,16 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 pub struct ClientArgs {
 
+    /// The amount of time used to process the result
+    #[clap(short = 't', long = "time", default_value_t = 1000)]
+    pub time: u32,
+    /// The exploration constant of the UCB1 formular
+    #[clap(short = 'c', long = "exploration_constant", default_value_t = 2.0)]
+    pub exploration_constant: f64,
+    /// The number of random simulatins per rollout
+    #[clap(short = 's', long = "n_simulations", default_value_t = 100)]
+    pub n_simulations: u32,
+
     /// The game server's host address.
     #[clap(short, long, default_value = "localhost")]
     pub host: String,
